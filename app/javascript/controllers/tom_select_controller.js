65789a3a0cf790ee38bfd52cd388792ab0ac9c20
Philip Curry <muttaphilly@gmail.com>
2023-04-01 10:25:47 +0800

    lunch save

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,9 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
-import TomSelect from "tom-select"
+import TomSelect from "tom-select";
 
-// Connects to data-controller="tom-select"
 export default class extends Controller {
   connect() {
-    new TomSelect(this.element, {})
+    new TomSelect(this.element)
   }
 }
