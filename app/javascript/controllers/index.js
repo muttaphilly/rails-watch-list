ea2f02bc78c3d0697e69cf7eacc88516c80bcabd
Philip Curry <muttaphilly@gmail.com>
2023-04-01 09:33:44 +0800

    Bug fixes - display movies, delete lists

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,11 +4,11 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import IndexController from "./index_controller"
 application.register("index", IndexController)
+
+import NavbarController from "./navbar_controller"
+application.register("navbar", NavbarController)
 
 import StarRatingController from "./star_rating_controller"
 application.register("star-rating", StarRatingController)
